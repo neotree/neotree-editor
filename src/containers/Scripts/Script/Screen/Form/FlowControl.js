92346fa7338612e1f5d58deca35ca08d9da02d7e
farai <farai@bwsonline.co.za>
2020-07-07 09:13:42 +0200

    Screen FlowControl form

--- src/containers/Scripts/Script/Screen/Form/FlowControl.js
+++ src/containers/Scripts/Script/Screen/Form/FlowControl.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useScreenContext } from '@/contexts/screen';
 import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Title from './Title';
 
 function FlowControl() {
@@ -11,12 +14,31 @@ function FlowControl() {
       <Title>FLOW CONTROL</Title>
 
       <div>
+        <FormControlLabel
+          control={(
+            <Switch
+              checked={!!form.skippable}
+              onChange={() => setForm(f => ({ skippable: !f.skippable }))}
+              name="skippable"
+            />
+          )}
+          label="Allow user to skip this screen"
+        />
+      </div>
+
+      <br />
+
+      <div>
         <TextField
           fullWidth
           value={form.condition || ''}
           label="Conditional expression"
           onChange={e => setForm({ condition: e.target.value })}
         />
+        <Typography
+          variant="caption"
+          color="textSecondary"
+        >Example: <b>($key = true and $key2 = false) or $key3 = 'HD'</b></Typography>
       </div>
 
       <br />
