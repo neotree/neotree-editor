b155d0d7e2e19a0c07ce6983546b055b92e6b125
farai <farai@bwsonline.co.za>
2020-07-09 08:55:55 +0200

    Project folders

--- src/_redux/store/index.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-if (isProd) {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
