a57e48198a64921ae16147c1d7cc765ee7e79ad6
farai <farai@bwsonline.co.za>
2020-10-23 11:17:18 +0200

    Device registration

--- server/routes/devices/getDeviceMiddleware.js
+++ server/routes/devices/getDeviceMiddleware.js
@@ -1,14 +1,37 @@
-import { Device } from '../../database';
+import { Device, } from '../../database';
+import { firebaseAdmin } from '../../firebase';
+
+function makeid(chars = '', length = 4) {
+  let result = '';
+  for (let i = 0; i < length; i++) result += chars.charAt(Math.floor(Math.random() * chars.length));
+  return result.toUpperCase();
+}
 
 module.exports = () => (req, res, next) => {
-  const payload = req.query;
+  (async () => {
+    const { deviceId } = req.query;
 
-  const done = (e, payload) => {
-    res.locals.setResponse(e, payload);
-    next();
-  };
+    const done = (e, payload) => {
+      res.locals.setResponse(e, payload);
+      next();
+    };
 
-  Device.findOne({ where: payload })
-    .then(device => done(null, { device }))
-    .catch(done);
+    if (!deviceId) return done(new Error('"deviceId" is not provided'));
+
+    let device = null;
+    try {
+      device = await Device.findOrCreate({
+        where: { device_id: deviceId },
+        defaults: {
+          device_id: deviceId,
+          device_hash: makeid(deviceId),
+          details: JSON.stringify({ scripts_count: 0, }),
+        },
+      });
+    } catch (e) { /* Do nothing */ }
+
+    try { await firebaseAdmin.database().ref(`devices/${deviceId}`).set(device); } catch (e) { /* Do nothing */ }
+
+    done(null, { device });
+  })();
 };
