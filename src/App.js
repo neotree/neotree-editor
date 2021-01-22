1dcb173f0be35ad8eb4bf878af9599f0979ad66b
L.Farai Matare <farai@bwsonline.co.za>
2021-01-22 20:59:50 +0200

    Prod build

--- src/App.js
+++ src/App.js
@@ -18,21 +18,7 @@ const App = () => {
   );
 
   React.useEffect(() => {
-    socket.on('create_scripts', data => console.log('socket: create_scripts', data)); // eslint-disable-line
-    socket.on('update_scripts', data => console.log('socket: update_scripts', data)); // eslint-disable-line
-    socket.on('delete_scripts', data => console.log('socket: delete_scripts', data)); // eslint-disable-line
-
-    socket.on('create_screens', data => console.log('socket: create_screens', data)); // eslint-disable-line
-    socket.on('update_screens', data => console.log('socket: update_screens', data)); // eslint-disable-line
-    socket.on('delete_screens', data => console.log('socket: delete_screens', data)); // eslint-disable-line
-
-    socket.on('create_diagnoses', data => console.log('socket: create_diagnoses', data)); // eslint-disable-line
-    socket.on('update_diagnoses', data => console.log('socket: update_diagnoses', data)); // eslint-disable-line
-    socket.on('delete_diagnoses', data => console.log('socket: delete_diagnoses', data)); // eslint-disable-line
-
-    socket.on('create_config_keys', data => console.log('socket: create_config_keys', data)); // eslint-disable-line
-    socket.on('update_config_keys', data => console.log('socket: update_config_keys', data)); // eslint-disable-line
-    socket.on('delete_config_keys', data => console.log('socket: delete_config_keys', data)); // eslint-disable-line
+    socket.on('data_updated', data => console.log('socket: data_updated', data)); // eslint-disable-line
   }, []);
 
   if (!appInitialised) return wrapComponents(<PageLoader />);
