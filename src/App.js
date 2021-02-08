270da7c3ae22bf86437f98d6b68bc474f61456a4
L.Farai Matare <farai@bwsonline.co.za>
2021-02-08 13:02:16 +0200

    Fix layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+/* global fetch, $APP */
 import React from 'react';
+import io from 'socket.io-client';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { provideAppContext, useAppContext } from '@/AppContext';
-import PageLoader from '@/components/PageLoader';
 import DocumentTitle from '@/components/DocumentTitle';
 import Containers from './containers';
+import AppContext from './AppContext';
+
+const socket = io();
 
 const theme = createMuiTheme({
   palette: {
@@ -17,22 +20,48 @@ const theme = createMuiTheme({
 });
 
 const App = () => {
-  const { socket, state: { appInitialised } } = useAppContext();
+  const [state, _setState] = React.useState({
+    documentTitle: '',
+    navSection: null,
+    initialisingApp: false,
+    ...(() => { try { return $APP; } catch (e) { return null; } })()
+  });
+  const setState = s => _setState(prev => ({
+    ...prev,
+    ...(typeof s === 'function' ? s(prev) : s),
+  }));
+
+  const getBackupStatus = () => new Promise((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch('/get-backup-status');
+        const { shouldBackup, appInfo } = await res.json();
+        setState({
+          shouldBackup,
+          version: appInfo ? appInfo.version : 1
+        });
+        resolve({ shouldBackup, appInfo });
+      } catch (e) { reject(e); }
+    })();
+  });
+
+  React.useEffect(() => {
+    getBackupStatus();
+    socket.on('data_updated', getBackupStatus);
+  }, []);
 
   React.useEffect(() => {
     socket.on('data_updated', data => console.log('socket: data_updated', data)); // eslint-disable-line
   }, []);
 
   return (
-    <MuiThemeProvider theme={theme}>
-      {!appInitialised ? <PageLoader /> : (
-        <>
-          <DocumentTitle />
-          <Containers />
-        </>
-      )}
-    </MuiThemeProvider>
+    <AppContext.Provider value={{ state, setState, socket }}>
+      <MuiThemeProvider theme={theme}>
+        <DocumentTitle />
+        <Containers />
+      </MuiThemeProvider>
+    </AppContext.Provider>
   );
 };
 
-export default provideAppContext(App);
+export default App;
--- src/AppContext.js
+++ src/AppContext.js
@@ -1,16 +1,5 @@
 /* global fetch, $APP */
 import React from 'react';
-import io from 'socket.io-client';
-
-const socket = io();
-
-const defaultState = {
-  documentTitle: '',
-  navSection: null,
-  appInitialised: true,
-  initialisingApp: false,
-  ...(() => { try { return $APP; } catch (e) { return null; } })()
-};
 
 export const AppContext = React.createContext(null);
 
@@ -32,37 +21,4 @@ export const setNavSection = navSection => {
   }, [navSection]);
 };
 
-export const provideAppContext = Component => function AppContextProvider(props) {
-  const [state, _setState] = React.useState(defaultState);
-  const setState = s => _setState(prev => ({
-    ...prev,
-    ...(typeof s === 'function' ? s(prev) : s),
-  }));
-
-  const getBackupStatus = () => new Promise((resolve, reject) => {
-    (async () => {
-      try {
-        const res = await fetch('/get-backup-status');
-        const { shouldBackup, appInfo } = await res.json();
-        setState({
-          shouldBackup,
-          version: appInfo ? appInfo.version : 1
-        });
-        resolve({ shouldBackup, appInfo });
-      } catch (e) { reject(e); }
-    })();
-  });
-
-  React.useEffect(() => {
-    getBackupStatus();
-    socket.on('data_updated', getBackupStatus);
-  }, []);
-
-  return (
-    <AppContext.Provider
-      value={{ state, _setState, setState, socket }}
-    >
-      <Component {...props} />
-    </AppContext.Provider>
-  );
-};
+export default AppContext;
