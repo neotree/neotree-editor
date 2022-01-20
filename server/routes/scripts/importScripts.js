8ccb9de09cd99c52fa15f2490b62dabf6dc3e70c
farai <farai@bwsonline.co.za>
2022-01-20 16:57:13 +0200

    Import scripts

--- server/routes/scripts/importScripts.js
+++ server/routes/scripts/importScripts.js
@@ -9,7 +9,7 @@ export function importScripts(app) {
     return (req, res, next) => {
         const { url, scriptId } = req.body;
         (async () => {
-            ((url || '').match('https') ? https : http).get(`${url}/get-import-scripts?scriptId=${scriptId}`, resp => {
+            (`${url}`.match('https') ? https : http).get(`${url}/get-import-scripts?scriptId=${scriptId}`, resp => {
                 let _data = '';
 
                 resp.on('data', (chunk) => {
@@ -17,15 +17,64 @@ export function importScripts(app) {
                 });
 
                 resp.on('end', () => {
-                    let data = null;
-                    let error = null;
-                    try { 
-                        data = _data ? JSON.parse(_data) : {}; 
-                    } catch (e) { 
-                        error = 'Failed to import, make sure the URL is correct';
-                    }
-                    res.locals.setResponse(error, { data });
-                    next();
+                    (async () => {
+                        let data = null;
+                        let error = null;
+                        let resp = null;
+
+                        try { 
+                            data = _data ? JSON.parse(_data) : {}; 
+                            
+                            try {
+                                resp = await Promise.all(Object.keys(data).map(key => new Promise(resolve => {
+                                    (async () => {
+                                        const { script, screens, diagnoses } = data[key];
+    
+                                        delete script.data.id;
+                                        const newScript = await createScript(script.data);
+    
+                                        if (newScript) {
+                                            const saveScreens = async (_screens = screens, saved = []) => {
+                                                const s = _screens.shift();
+                                                try {
+                                                    delete s.data.id;
+                                                    const newScreen = await createScreen({  
+                                                        ...s.data,
+                                                        scriptId: newScript.script_id, 
+                                                    });
+                                                    if (newScreen) saved.push(newScreen);
+                                                } catch(e) { /* */ }
+                                                if (_screens.length) saved = await saveScreens(_screens, saved);
+                                                return saved;
+                                            };
+                                            await saveScreens();
+    
+                                            const saveDiagnoses = async (_diagnoses = diagnoses, saved = []) => {
+                                                const d = _diagnoses.shift();
+                                                try {
+                                                    delete d.data.id;
+                                                    const newScreen = await createDiagnosis({  
+                                                        ...d.data,
+                                                        scriptId: newScript.script_id, 
+                                                    });
+                                                    if (newScreen) saved.push(newScreen);
+                                                } catch(e) { /* */ }
+                                                if (_diagnoses.length) saved = await saveDiagnoses(_diagnoses, saved);
+                                                return saved;
+                                            };
+                                            await saveDiagnoses();
+                                        }
+    
+                                        resolve(newScript);
+                                    })();
+                                })));
+                            } catch (e) { error = e.message; }
+                        } catch (e) { 
+                            error = 'Failed to import, make sure the URL is correct';
+                        }
+                        res.locals.setResponse(error, { data: resp, });
+                        next();
+                    })();
                 });
             }).on('error', e => {
                 res.locals.setResponse(e.message);
@@ -35,61 +84,31 @@ export function importScripts(app) {
     };
 }
 
-export function getImportScripts(app) {
+export function getImportScripts() {
     return (req, res, next) => {
         const { scriptId } = req.query;
         (async () => {
             const where = { deletedAt: null, };
             if (scriptId) where.script_id = scriptId;
             const scripts = await Script.findAll({ where });
-            const data = await Promise.all(scripts.map(s => new Promise((resolve) => {
-            (async () => {                
-                const screens = await Screen.findAll({ 
-                    where: { deletedAt: null, script_id: s.script_id, },
-                    order: [['position', 'ASC']],
-                });
-                const diagnoses = await Diagnosis.findAll({ 
-                    where: { deletedAt: null, script_id: s.script_id, }, 
-                    order: [['position', 'ASC']],
-                });
+            const data = {};
+            await Promise.all(scripts.map(script => new Promise((resolve) => {
+                (async () => {                
+                    const screens = await Screen.findAll({ 
+                        where: { deletedAt: null, script_id: script.script_id, },
+                        order: [['position', 'ASC']],
+                    });
+                    const diagnoses = await Diagnosis.findAll({ 
+                        where: { deletedAt: null, script_id: script.script_id, }, 
+                        order: [['position', 'ASC']],
+                    });
+                    data[script.script_id] = {};
+                    data[script.script_id].script = script;
+                    data[script.script_id].screens = screens;
+                    data[script.script_id].diagnoses = diagnoses;
 
-                delete s.data.id;
-                const newScript = await createScript(s.data);
-
-                if (newScript) {
-                    const saveScreens = async (_screens = screens, saved = []) => {
-                        const s = _screens.shift();
-                        try {
-                            delete s.data.id;
-                            const newScreen = await createScreen({  
-                                ...s.data,
-                                scriptId: newScript.script_id, 
-                            });
-                            if (newScreen) saved.push(newScreen);
-                        } catch(e) { /* */ }
-                        if (_screens.length) saved = await saveScreens(_screens, saved);
-                        return saved;
-                    };
-                    await saveScreens();
-
-                    const saveDiagnoses = async (_diagnoses = diagnoses, saved = []) => {
-                        const d = _diagnoses.shift();
-                        try {
-                            delete d.data.id;
-                            const newScreen = await createDiagnosis({  
-                                ...d.data,
-                                scriptId: newScript.script_id, 
-                            });
-                            if (newScreen) saved.push(newScreen);
-                        } catch(e) { /* */ }
-                        if (_diagnoses.length) saved = await saveDiagnoses(_diagnoses, saved);
-                        return saved;
-                    };
-                    await saveDiagnoses();
-                }
-
-                resolve(newScript);
-            })();
+                    resolve();
+                })();
             })));
             res.locals.setResponse(null, data);
             next();
