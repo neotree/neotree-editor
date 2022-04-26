8ff79247b7f47b4fa1c768fa6c6e7332a4a41c01
farai <farai.matare@bwscloud.tech>
2022-04-26 16:28:18 +0200

    Countly - add stats

--- server/routes/addStatsMiddleware.js
+++ server/routes/addStatsMiddleware.js
@@ -10,13 +10,15 @@ export default (req, res) => {
     const stats = req.body.stats || [];
     // Countly.begin_session();
     stats.forEach(stat => {
-        Countly.add_event({
-            key: stat.type,
-            count: stat.count,
-            // sum: 0,
-            dur: stat.duration,
-            segmentation: { ...stat.data }
-        });
+        Countly.track_view(stat.data.screenTitle || stat.data.screenId);
+        // Countly.add_event({
+        //     key: stat.type,
+        //     count: stat.count,
+        //     // sum: 0,
+        //     dur: stat.duration,
+        //     segmentation: { ...stat.data }
+        // });
     });
+    // Countly.end_session();
     res.json({ success: true });
 };
