777272c3264ac005bee2adc8ed0d12a599652e36
= <=>
2022-06-21 15:12:29 +0200

    Countly

--- server/routes/addStatsMiddleware.js
+++ server/routes/addStatsMiddleware.js
@@ -1,35 +1,42 @@
-import Countly from 'countly-sdk-nodejs';
+// import Countly from 'countly-sdk-nodejs';
 import * as database from '../database';
+
+var Countly = require("countly-sdk-nodejs").Bulk;
 
 const hasEnvVariables = process.env.COUNTLY_APP_KEY && process.env.COUNTLY_HOST;
 
+let countlyServer = null;
 if (hasEnvVariables) {
-    Countly.init({
+    countlyServer = new Countly({
         app_key: process.env.COUNTLY_APP_KEY,
         url: process.env.COUNTLY_HOST,
         debug: true
     });
 }
 
+countlyServer.start();
+
 export default (req, res) => {
     (async () => {
         const appInfo = await database.App.findOne({ where: { id: 1 } });
 
-        if (hasEnvVariables && appInfo && !appInfo.should_track_usage) return res.json({ success: true });
+        if (countlyServer && hasEnvVariables && appInfo && !appInfo.should_track_usage) return res.json({ success: true });
 
         const stats = req.body.stats || [];
-        // Countly.begin_session();
-        stats.forEach(stat => {
-            Countly.track_view(stat.data.screenTitle || stat.data.screenId);
-            // Countly.add_event({
-            //     key: stat.type,
-            //     count: stat.count,
-            //     // sum: 0,
-            //     dur: stat.duration,
-            //     segmentation: { ...stat.data }
-            // });
+
+        console.log(stats);
+
+        countlyServer.add_request({ 
+            begin_session: 1, 
+            // metrics:{ _os:"Linux" }, 
+            // device_id: "users_device_id", 
+            events: stats.map(stat => ({
+                key: stat.data.screenTitle || stat.data.screenId,
+                dur: stat.duration,
+                count: stat.count,
+            })),
         });
-        // Countly.end_session();
+
         res.json({ success: true });
     })();
 };
