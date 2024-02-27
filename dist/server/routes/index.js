3343deebc46dee9d60ea0b98c4a37bf6bc603459
farai <farai>
2024-02-27 17:46:46 +0200

    prod build

--- dist/server/routes/index.js
+++ dist/server/routes/index.js
@@ -7,10 +7,13 @@ var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/h
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 var _express = _interopRequireDefault(require("express"));
+var _http = _interopRequireDefault(require("http"));
+var _https = _interopRequireDefault(require("https"));
 var database = _interopRequireWildcard(require("../database"));
 var _backup = require("../utils/backup");
 var _addStatsMiddleware = require("./addStatsMiddleware");
-var _excluded = ["id"];
+var _excluded = ["data"],
+  _excluded2 = ["id"];
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 (function () {
@@ -63,91 +66,124 @@ module.exports = function (app) {
       }, _callee, null, [[0, 10]]);
     }))();
   });
-  router.get('/test-countly', _addStatsMiddleware.testCounty);
-  router.post('/update-app-info', function (req, res) {
-    var _req$body = req.body,
-      id = _req$body.id,
-      payload = (0, _objectWithoutProperties2["default"])(_req$body, _excluded);
+  router.get('/remove-confidential-data', function (req, res) {
     (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2() {
-      var appInfo;
+      var screensRes, screens, confidentialKeysMap, keys;
       return _regenerator["default"].wrap(function _callee2$(_context2) {
         while (1) switch (_context2.prev = _context2.next) {
           case 0:
             _context2.prev = 0;
-            _context2.next = 3;
+            console.log('getting screens...');
+            _context2.next = 4;
+            return database.Screen.findAll({
+              where: {
+                deletedAt: null
+              }
+            });
+          case 4:
+            screensRes = _context2.sent;
+            screens = screensRes.map(function (screen) {
+              var _JSON$parse = JSON.parse(JSON.stringify(screen)),
+                data = _JSON$parse.data,
+                s = (0, _objectWithoutProperties2["default"])(_JSON$parse, _excluded);
+              return _objectSpread(_objectSpread({}, data), s);
+            });
+            console.log(screens.length + ' screens found...');
+            confidentialKeysMap = screens.reduce(function (acc, item) {
+              var metadata = _objectSpread({
+                fields: [],
+                items: []
+              }, item.metadata);
+              var fields = metadata.fields;
+              var items = metadata.items;
+              fields.forEach(function (f) {
+                if (f.confidential) acc[f.key] = true;
+              });
+              items.forEach(function (f) {
+                if (f.confidential) acc[f.key] = true;
+              });
+              if (metadata.confidential && metadata.key) acc[metadata.key] = true;
+              return acc;
+            }, {});
+            keys = Object.keys(confidentialKeysMap);
+            res.json({
+              keys: keys
+            });
+            // console.log('posting to ' + `${process.env.NODEAPI_URL}/remove-confidential-data`, { keys });
+            // const _fetch = `${process.env.NODEAPI_URL}`.substring(0, 5) === 'https' ? https : http;
+            // _fetch.request(`${process.env.NODEAPI_URL}/remove-confidential-data`, { 
+            // 	method: 'POST', 
+            // 	body: JSON.stringify({ keys }),
+            // 	headers: { 'Content-type': 'application/json; charset=UTF-8', Connection: 'keep-alive', }, 
+            // }, (resp) => {
+            // 	// The whole response has been received. Print out the result.
+            // 	resp.on('end', () => {
+            // 		res.json({ success: true, });
+            // 	});
+
+            // }).on("error", (error) => {
+            // 	res.json({ error });
+            // });
+            _context2.next = 15;
+            break;
+          case 12:
+            _context2.prev = 12;
+            _context2.t0 = _context2["catch"](0);
+            res.json({
+              errors: [_context2.t0.message]
+            });
+          case 15:
+          case "end":
+            return _context2.stop();
+        }
+      }, _callee2, null, [[0, 12]]);
+    }))();
+  });
+  router.get('/test-countly', _addStatsMiddleware.testCounty);
+  router.post('/update-app-info', function (req, res) {
+    var _req$body = req.body,
+      id = _req$body.id,
+      payload = (0, _objectWithoutProperties2["default"])(_req$body, _excluded2);
+    (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee3() {
+      var appInfo;
+      return _regenerator["default"].wrap(function _callee3$(_context3) {
+        while (1) switch (_context3.prev = _context3.next) {
+          case 0:
+            _context3.prev = 0;
+            _context3.next = 3;
             return database.App.update(_objectSpread({}, payload), {
               where: {
                 id: id
               }
             });
           case 3:
-            _context2.next = 5;
+            _context3.next = 5;
             return database.App.findOne({
               where: {
                 id: id
               }
             });
           case 5:
-            appInfo = _context2.sent;
+            appInfo = _context3.sent;
             res.json({
               appInfo: appInfo
             });
-            _context2.next = 12;
+            _context3.next = 12;
             break;
           case 9:
-            _context2.prev = 9;
-            _context2.t0 = _context2["catch"](0);
-            res.json({
-              errors: [_context2.t0.message]
-            });
-          case 12:
-          case "end":
-            return _context2.stop();
-        }
-      }, _callee2, null, [[0, 9]]);
-    }))();
-  });
-  router.post('/publish-changes', function (req, res) {
-    (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee3() {
-      var appInfo, _shouldBackup;
-      return _regenerator["default"].wrap(function _callee3$(_context3) {
-        while (1) switch (_context3.prev = _context3.next) {
-          case 0:
-            _context3.prev = 0;
-            _context3.next = 3;
-            return (0, _backup.backupData)(app, req);
-          case 3:
-            _context3.next = 5;
-            return database.App.findOne({
-              id: 1
-            });
-          case 5:
-            appInfo = _context3.sent;
-            _context3.next = 8;
-            return (0, _backup.shouldBackup)();
-          case 8:
-            _shouldBackup = _context3.sent;
-            app.io.emit('data_published');
-            res.json({
-              shouldBackup: _shouldBackup,
-              appInfo: appInfo
-            });
-            _context3.next = 16;
-            break;
-          case 13:
-            _context3.prev = 13;
+            _context3.prev = 9;
             _context3.t0 = _context3["catch"](0);
             res.json({
               errors: [_context3.t0.message]
             });
-          case 16:
+          case 12:
           case "end":
             return _context3.stop();
         }
-      }, _callee3, null, [[0, 13]]);
+      }, _callee3, null, [[0, 9]]);
     }))();
   });
-  router.post('/discard-changes', function (req, res) {
+  router.post('/publish-changes', function (req, res) {
     (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4() {
       var appInfo, _shouldBackup;
       return _regenerator["default"].wrap(function _callee4$(_context4) {
@@ -155,7 +191,7 @@ module.exports = function (app) {
           case 0:
             _context4.prev = 0;
             _context4.next = 3;
-            return (0, _backup.restoreBackup)(app);
+            return (0, _backup.backupData)(app, req);
           case 3:
             _context4.next = 5;
             return database.App.findOne({
@@ -167,7 +203,7 @@ module.exports = function (app) {
             return (0, _backup.shouldBackup)();
           case 8:
             _shouldBackup = _context4.sent;
-            app.io.emit('changes_discarded');
+            app.io.emit('data_published');
             res.json({
               shouldBackup: _shouldBackup,
               appInfo: appInfo
@@ -185,6 +221,46 @@ module.exports = function (app) {
             return _context4.stop();
         }
       }, _callee4, null, [[0, 13]]);
+    }))();
+  });
+  router.post('/discard-changes', function (req, res) {
+    (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5() {
+      var appInfo, _shouldBackup;
+      return _regenerator["default"].wrap(function _callee5$(_context5) {
+        while (1) switch (_context5.prev = _context5.next) {
+          case 0:
+            _context5.prev = 0;
+            _context5.next = 3;
+            return (0, _backup.restoreBackup)(app);
+          case 3:
+            _context5.next = 5;
+            return database.App.findOne({
+              id: 1
+            });
+          case 5:
+            appInfo = _context5.sent;
+            _context5.next = 8;
+            return (0, _backup.shouldBackup)();
+          case 8:
+            _shouldBackup = _context5.sent;
+            app.io.emit('changes_discarded');
+            res.json({
+              shouldBackup: _shouldBackup,
+              appInfo: appInfo
+            });
+            _context5.next = 16;
+            break;
+          case 13:
+            _context5.prev = 13;
+            _context5.t0 = _context5["catch"](0);
+            res.json({
+              errors: [_context5.t0.message]
+            });
+          case 16:
+          case "end":
+            return _context5.stop();
+        }
+      }, _callee5, null, [[0, 13]]);
     }))();
   });
   router.get('/get-view-mode', function (req, res) {
