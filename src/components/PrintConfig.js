92103dbc9f84ef6402ec41e2877245ef6d48353f
farai <lafarai.dev@gmail.com>
2024-07-04 10:06:37 +0200

    printa config

--- src/components/PrintConfig.js
+++ src/components/PrintConfig.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
@@ -9,15 +10,20 @@ export function PrintConfig({ data: form, onChange }) {
     return (
         <>
             <Grid alignItems="center">
-                <FormControlLabel
-                    checked={form.printable}
-                    label="Print"
-                    control={<Checkbox />}
-                    onChange={(e) => onChange({ 
-                        ...form,
-                        printable: !form.printable 
-                    })}
-                />
+                <div>
+                    <FormControlLabel
+                        checked={form.printable}
+                        label="Print"
+                        control={<Checkbox />}
+                        onChange={(e) => onChange({ 
+                            ...form,
+                            printable: !form.printable 
+                        })}
+                    />
+                </div>
+                <Typography variant="caption" color="textSecondary">
+                    If not checked, data will not be display on the session summary and the printout.
+                </Typography>
             </Grid>
         </>
     )
