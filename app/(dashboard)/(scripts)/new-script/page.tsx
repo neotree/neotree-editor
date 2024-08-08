b7874f3f40d74e3e6128cb8d4afa657223c5c888
farai <lafarai.dev@gmail.com>
2024-08-08 22:18:12 +0200

    scripts

---
+++ app/(dashboard)/(scripts)/new-script/page.tsx
@@ -0,0 +1,25 @@
+import { Title } from "@/components/title";
+import { ScriptForm } from "../components/script-form";
+import { PageContainer } from "../components/page-container";
+import { getHospitals } from "@/app/actions/hospitals";
+
+type Props = {
+    searchParams: { [key: string]: string; };
+};
+
+export default async function NewScriptPage({}: Props) {
+    const hospitals = await getHospitals();
+
+    return (
+        <>
+            <Title>New Script</Title>
+
+            <PageContainer
+                title="New script"
+                backLink="/"
+            >
+                <ScriptForm hospitals={hospitals.data} />
+            </PageContainer>
+        </>
+    )
+}
