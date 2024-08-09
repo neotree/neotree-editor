d9a49a2dcb888e1d045e5288cf78cbd06cf578cd
farai <lafarai.dev@gmail.com>
2024-08-09 18:39:42 +0200

    sessions

--- app/(dashboard)/sessions/[sessionId]/page.tsx
+++ app/(dashboard)/sessions/[sessionId]/page.tsx
@@ -1,9 +1,9 @@
 import { getSession } from "@/app/actions/sessions";
+import { getScript } from "@/app/actions/scripts";
 import { Title } from "@/components/title";
 import { Alert } from "@/components/alert";
 
-import { Container } from "../components/container";
-import { Session } from "../components/session";
+import { SessionForm } from "../components/session-form";
 
 type Props = {
     params: { sessionId: string; },
@@ -14,6 +14,10 @@ export default async function SessionsPage({ params: { sessionId, } }: Props) {
     const [session] = await Promise.all([
         getSession(Number(sessionId)),
     ]);
+
+    const { data: script } = !session?.data?.scriptid ? { data: null } : await getScript({
+        scriptId: session?.data?.scriptid,
+    })
 
     if (!session.data) {
         return (
@@ -31,8 +35,9 @@ export default async function SessionsPage({ params: { sessionId, } }: Props) {
         <>
             <Title>Session</Title>
 
-            <Session 
+            <SessionForm 
                 session={session}
+                script={script}
                 getSession={getSession}
             />
         </>
