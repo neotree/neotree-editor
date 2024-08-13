db76d4f9253c284af1bce8627192c9b90a842c7b
farai <lafarai.dev@gmail.com>
2024-08-13 18:33:05 +0200

    sockets

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 
-console.log('process.env.NEXT_PUBLIC_APP_URL', process.env.NEXT_PUBLIC_APP_URL);
-
 const socket = io(process.env.NEXT_PUBLIC_APP_URL);
 
 export default socket;
