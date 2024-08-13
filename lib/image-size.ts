2b8cf98c5c0e535cc99408a79467dfd0f0bcc4f2
farai <lafarai.dev@gmail.com>
2024-08-13 15:01:34 +0200

    remote image size

--- lib/image-size.ts
+++ lib/image-size.ts
@@ -2,28 +2,36 @@ import http from 'node:http';
 import https from 'node:https';
 import sizeOf from 'image-size';
 
-export function getRemoteImageSize(url: string) {
-    return new Promise<{ width: number; height: number; }>((resolve, reject) => {
-        const _http = `${url || ''}`.substring(0, 5) === 'https' ? https : http;
-        _http.get(new URL(url), function (response) {
-            const chunks: any[] = []
-            response.on('data', function (chunk) {
-                chunks.push(chunk)
-            })
-            .on('end', function () {
-                const buffer = Buffer.concat(chunks);
-                try {
-                    const res = sizeOf(buffer, function(e) {
-                        if (e) reject(e);
-                    });
-                    resolve(res);
-                } catch(e) {
+export async function getRemoteImageSize(url: string) {
+    try {
+        const chunks: any[] = []
+
+        const res = await new Promise<{ width: number; height: number; }>((resolve, reject) => {
+            const _http = `${url || ''}`.substring(0, 5) === 'https' ? https : http;
+            _http.get(new URL(url), function (response) {
+                
+                response.on('data', function (chunk) {
+                    chunks.push(chunk)
+                })
+                .on('end', function () {
+                    const buffer = Buffer.concat(chunks);
+                    try {
+                        const res = sizeOf(buffer, function(e) {
+                            if (e) reject(e);
+                        });
+                        resolve(res);
+                    } catch(e) {
+                        reject(e);
+                    }
+                })
+                .on('error', function(e) {
                     reject(e);
-                }
-            })
-            .on('error', function(e) {
-                reject(e);
+                });
             });
         });
-    });
+        
+        return res;
+    } catch(e) {
+        throw e;
+    }
 }
