12377e998edde26afe725228c0a4122341714c9e
farai <lafarai.dev@gmail.com>
2024-08-15 16:09:27 +0200

    prod build

---
+++ build/static/chunks/app/_not-found/page-144cc02694f2a055.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7409,8875,7330,6186,2449,3249,2961,9628,9219,1443,8705,1227,998,5530,2943,634,4566,6900],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
