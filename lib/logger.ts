ca7c6ab0edb361314754f4e6285df135eda7be87
farai <lafarai.dev@gmail.com>
2024-08-15 16:08:00 +0200

    logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,38 +2,40 @@ import fs from 'node:fs';
 import path from 'node:path';
 import moment from 'moment';
 
-function writeLogsToFile(type: 'error' | 'log', ...args: any[]) {
+function writeLogsToFile(filename: string, ...args: any[]) {
     const date = moment(new Date()).format('YYYYMMDD');
     const dir = path.resolve(`logs/${date}`);
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-    const data = `${new Date().toUTCString()} ${JSON.stringify([...args, __filename])}\n`;
-    let filename = `${dir}/logs.txt`;
-    if (type === 'error') filename = `${dir}/errors.txt`; 
-    fs.appendFileSync(filename, data);
+    const data = `${new Date().toUTCString()} ${JSON.stringify([...args])}\n`;
+    const filePath = `${dir}/${filename}`;
+    fs.appendFileSync(filePath, data);
 }
 
 const log: typeof console.log = (...args) => {
     if (process.env.NODE_ENV !== 'production') {
         console.log(...args, __filename);
     }
-    writeLogsToFile('log', ...args);
+    writeLogsToFile('logs.txt', ...args);
 };
 
 const error: typeof console.error = (...args) => {
-    try {
-        if (process.env.NODE_ENV !== 'production') {
-            console.error(...args, __filename);
-        }
-        writeLogsToFile('error', ...args);
-    } catch(e) {
-        console.log(e);
-        // do nothing
+    if (process.env.NODE_ENV !== 'production') {
+        console.error(...args, __filename);
     }
+    writeLogsToFile('errors.txt', ...args);
+};
+
+const appError: typeof console.error = (...args) => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.error(...args, __filename);
+    }
+    writeLogsToFile('app_errors.txt', ...args);
 };
 
 const logger = {
     log,
     error,
+    appError,
 };
 
 export default logger;
