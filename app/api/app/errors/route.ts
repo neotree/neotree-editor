5adc5aed11fc654a399f5c865079d488992edf2d
farai <lafarai.dev@gmail.com>
2024-08-15 15:40:25 +0200

    log app errors

---
+++ app/api/app/errors/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import logger from "@/lib/logger";
+import { isAuthenticated } from "@/app/actions/is-authenticated";
+import { _getFullUser } from "@/databases/queries/users";
+import { _saveDevices } from "@/databases/mutations/devices";
+
+export async function POST(req: NextRequest) {
+	try {
+        const isAuthorised = await isAuthenticated();
+
+        if (!isAuthorised.yes) return NextResponse.json({ errors: ['Unauthorised'], });
+
+        const body = await req.json();
+
+        logger.error('[APP ERROR]: ', body);
+
+		return NextResponse.json({ success: true, });
+	} catch(e: any) {
+		logger.error('[POST] /app/errors', e.message);
+		return NextResponse.json({ errors: ['Internal Error'] });
+	}
+}
