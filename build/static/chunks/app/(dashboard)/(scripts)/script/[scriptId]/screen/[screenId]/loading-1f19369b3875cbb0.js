411a6c79262f67abd7a0927cb3a1a27ac96c7042
farai <lafarai.dev@gmail.com>
2024-08-27 21:01:13 +0200

    prod build

--- build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/screen/[screenId]/loading-1f19369b3875cbb0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1443,7409,8875,7330,6186,2961,8705,2449,3249,6900,9628,9219,2943,1227,998,4566,634,5530],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
