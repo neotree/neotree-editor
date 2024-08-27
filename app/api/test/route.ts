1d737249e10b531e8c58270a735b527e2b59d38e
farai <lafarai.dev@gmail.com>
2024-08-27 20:23:46 +0200

    prod build

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -8,28 +8,7 @@ export async function GET(req: NextRequest) {
 	try {
         const reqQuery = queryString.parse(req.nextUrl.searchParams.toString());
 
-        let data: any = {
-            NODE_ENV: process.env.NODE_ENV,
-            NEOTREE_SERVER_TYPE: process.env.NEOTREE_SERVER_TYPE,
-            NEOTREE_ENV: process.env.NEOTREE_ENV,
-            HOSTNAME: process.env.HOSTNAME,
-            PORT: process.env.PORT,
-            DEBUG: process.env.DEBUG,
-            POSTGRES_DB_URL: process.env.POSTGRES_DB_URL,
-            SESSIONS_DB_URL: process.env.SESSIONS_DB_URL,
-            MAIL_MAILER: process.env.MAIL_MAILER,
-            MAIL_HOST: process.env.MAIL_HOST,
-            MAIL_PORT: process.env.MAIL_PORT,
-            MAIL_USERNAME: process.env.MAIL_USERNAME,
-            MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-            MAIL_ENCRYPTION: process.env.MAIL_ENCRYPTION,
-            MAIL_FROM_ADDRESS: process.env.MAIL_FROM_ADDRESS,
-            MAIL_FROM_NAME: process.env.MAIL_FROM_NAME,
-            NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-            NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-            NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-            NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        };
+        let data: any = {};
 
         if (reqQuery.email) {
             const user = await _getUser(reqQuery.email as string);
--- build/static/chunks/app/(dashboard)/sessions/loading-c1df0df992295a8a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6186,7409,8875,7330,2943,2449,3249,6900,9628,9219,1227,998,4566,1443,8705,634,2961,5530],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
