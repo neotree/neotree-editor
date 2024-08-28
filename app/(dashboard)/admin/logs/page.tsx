aff32efc6e45020ed72fdcc8d93709b55a7c7dca
farai <lafarai.dev@gmail.com>
2024-08-28 19:38:01 +0200

    prod build

--- app/(dashboard)/admin/logs/page.tsx
+++ app/(dashboard)/admin/logs/page.tsx
@@ -3,17 +3,12 @@ import { Title } from "@/components/title";
 import { Content } from "./components/content";
 
 export default async function AdminLogsPage() {
-    const _getLogs: typeof getLogs = async (...params) => {
-        'use server';
-        return getLogs(...params);
-    };
-
     return (
         <>
             <Title>Logs</Title>
 
             <Content 
-                _getLogs={_getLogs}
+                _getLogs={getLogs}
             />
         </>
     );
