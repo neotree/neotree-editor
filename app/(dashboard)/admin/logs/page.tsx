b0bdb801117579d55bfbf875c7878b030d5b1374
farai <lafarai.dev@gmail.com>
2024-08-28 17:48:58 +0200

    prod build

--- app/(dashboard)/admin/logs/page.tsx
+++ app/(dashboard)/admin/logs/page.tsx
@@ -3,12 +3,17 @@ import { Title } from "@/components/title";
 import { Content } from "./components/content";
 
 export default async function AdminLogsPage() {
+    const _getLogs: typeof getLogs = async (...params) => {
+        'use server';
+        return getLogs(...params);
+    };
+
     return (
         <>
             <Title>Logs</Title>
 
             <Content 
-                _getLogs={getLogs}
+                _getLogs={_getLogs}
             />
         </>
     );
---
+++ build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/screen/loading-5b40365c0d1e135e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8705,7409,8875,7330,6186,4566,998,2961,5530,1227,1443,2449,3249,6900,2943,634,9628,9219],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
