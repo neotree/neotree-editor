df5e35258f726b9e5f0dbc03ba7afbaec948d075
farai <lafarai.dev@gmail.com>
2024-08-28 19:42:47 +0200

    lint

--- app/(dashboard)/admin/logs/error.tsx
+++ app/(dashboard)/admin/logs/error.tsx
@@ -3,11 +3,10 @@
 import { useEffect } from "react";
 import { RefreshCcw } from "lucide-react";
 
-import { Title } from "@/components/title";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
-export default async function ErrorPage({ error, reset }: {
+export default function ErrorPage({ error, reset }: {
     error: Error & { digest?: string; };
     reset: () => void;
 }) {
@@ -18,8 +17,6 @@ export default async function ErrorPage({ error, reset }: {
 
     return (
         <>
-            <Title>Logs ERROR</Title>
-
             <Card>
                 <CardContent className="px-4 py-4 flex flex-col justify-center items-center gap-y-4">
                     <div className="text-3xl">Something went wrong</div>
