001d8e48cd4d2b833c9b49921838678f058560cf
farai <lafarai.dev@gmail.com>
2024-08-29 07:58:21 +0200

    sign up

--- app/(dashboard)/components/navigation/top-bar.tsx
+++ app/(dashboard)/components/navigation/top-bar.tsx
@@ -114,7 +114,7 @@ export function TopBar({}: Props) {
                         Switch to <b>{mode === 'development' ? 'view' : 'development'}</b> mode
                     </a>
 
-                    {shouldPublishData && (isSuperUser || isAdmin) && (
+                    {(mode === 'development') && shouldPublishData && (isSuperUser || isAdmin) && (
                         <>
                             <Button
                                 variant="destructive"
