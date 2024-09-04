5e655ba50a393ad2279c6ff3ef503fcaa3353927
farai <lafarai.dev@gmail.com>
2024-09-04 10:20:30 +0200

    prod build

---
+++ app/api/ops/publish-data/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { isAuthenticated } from "@/app/actions/is-authenticated";
+import { publishData } from "@/app/actions/ops";
+import logger from "@/lib/logger";
+
+export async function POST(req: NextRequest) {
+    try {
+        const isAuthorised = await isAuthenticated();
+
+        if (!isAuthorised.yes) return NextResponse.json({ errors: ['Unauthorised'], }, { status: 200, });
+        
+        const data = await publishData();
+
+        return NextResponse.json(data);
+    } catch(e: any) {
+        logger.log('/api/ops/publish-data', e);
+        return NextResponse.json({ errors: [e.message], });
+    }
+}
--- build/static/chunks/app/(dashboard)/admin/logs/loading-a796dec1b7cd5b2c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5530,7409,8875,7330,6186,9628,9219,2449,3249,2961,6900,1227,998,1443,8705,634,4566,2943],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
