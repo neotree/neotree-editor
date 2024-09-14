5c66b508888fff67dc19c950a850c63cac1974c2
farai <lafarai.dev@gmail.com>
2024-09-14 17:39:06 +0200

    preferences

--- build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/screen/loading-44b3cf173c6a9a28.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8705,7409,8875,7330,6186,2961,9628,9219,2449,3249,1227,998,4566,5530,1443,634,2943,6900],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
---
+++ build/static/chunks/app/(dashboard)/admin/loading-a44306b71c2c29fd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[998,7409,6186,7330,8875,1443,8705,2449,3249,2943,2961,5530,6900,1227,634,9628,9219,4566],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
