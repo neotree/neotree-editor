69dbd0e5b855f7d1abe5ab7962008c7f3ab47709
farai <lafarai.dev@gmail.com>
2024-09-14 17:50:14 +0200

    prod build

--- build/static/chunks/app/(dashboard)/account/loading-88d463586c9f3bee.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8875,7409,6186,7330,1443,8705,2449,3249,2943,2961,5530,998,6900,1227,634,9628,9219,4566],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
