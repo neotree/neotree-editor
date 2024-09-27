90b829da0702f5037633fe24ad7cd3fdb1bc9290
farai <lafarai.dev@gmail.com>
2024-09-27 10:14:12 +0200

    copy scripts

--- build/static/chunks/app/(dashboard)/(scripts)/new-script/loading-9d343b36283473d9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2961,7409,8875,7330,6186,2943,2449,3249,9628,9219,6900,1443,8705,1227,998,4566,634,5530],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- server/env.js
+++ server/env.js
@@ -2,6 +2,12 @@ try {
     require('dotenv').config({ path: '.env.local', });
 } catch(e) {
     // do nothing
+}
+
+try {
+    require('dotenv').config({ path: '.env.development', });
+} catch(e) {
+    // do nothing
 } finally {
     require('dotenv/config');
 }
