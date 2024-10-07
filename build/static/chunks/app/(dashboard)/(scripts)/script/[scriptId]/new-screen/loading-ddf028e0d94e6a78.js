8f62c959ed018375ca2e39239f14e7a5c2ff0a4e
farai <lafarai.dev@gmail.com>
2024-10-07 10:14:47 +0200

    screens position

---
+++ build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/new-screen/loading-ddf028e0d94e6a78.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6900,7409,8875,7330,6186,2449,3249,9628,9219,2943,2961,1227,998,634,1443,8705,4566,5530],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
