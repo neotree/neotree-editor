22634647ec23d696d1e6f4abb6a01bd8b9540e5b
farai <lafarai.dev@gmail.com>
2024-10-07 12:32:46 +0200

    prod build

--- build/static/chunks/app/(dashboard)/loading-d8c40693f5f05df1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7330,7409,8875,6186,2943,2449,3249,6900,9628,9219,1443,8705,5530,998,1227,634,2961,4566],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
