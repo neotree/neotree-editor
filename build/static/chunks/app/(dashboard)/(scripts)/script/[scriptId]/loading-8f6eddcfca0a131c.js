a5aea22375f814bab2ef4272cc9315fa94d632a0
farai <lafarai.dev@gmail.com>
2024-10-08 09:29:20 +0200

    script hospitals

---
+++ build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/loading-8f6eddcfca0a131c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2449,7409,8875,7330,6186,1443,8705,3249,9628,9219,2943,2961,6900,1227,998,5530,634,4566],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
