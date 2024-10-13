8816402cbdf02c752e18f996c270d26d70c020cc
farai <farai>
2024-10-13 17:58:47 +0200

    fix - screens text field

--- build/server/webpack-runtime.js
+++ build/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b482f3f5925546e1")
+/******/ 		__webpack_require__.h = () => ("e541637c41a875d2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
