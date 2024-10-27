4b20a9a4618c4926a8894a78a0a82e8eeab2af3f
farai <farai>
2024-10-27 19:00:49 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/(scripts)/script/loading-fd5864b681411b84.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3249,7409,7330,8875,6186,2961,2449,2943,1443,8705,6900,9628,9219,6317,3146,903,1473],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
