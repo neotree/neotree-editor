f5311179dc224e48ac7862d5a14e66a46def7cd1
farai <farai>
2024-10-27 15:29:22 +0200

    fix - publish scripts

--- build/static/chunks/app/(dashboard)/sessions/loading-3b8a299c3455e6cc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6186,7409,7330,8875,2943,2449,3249,1443,8705,2961,9628,9219,3146,6317,6900,1473,903],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- build/types/app/api/scripts/route.ts
+++ build/types/app/api/scripts/route.ts
@@ -1,4 +1,4 @@
-// File: /Users/lafarai/Werq/BWS/NeoTree/neotree-webeditor/app/api/scripts/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/scripts/route.ts
 import * as entry from '../../../../../app/api/scripts/route.js'
 import type { NextRequest } from 'next/server.js'
 
