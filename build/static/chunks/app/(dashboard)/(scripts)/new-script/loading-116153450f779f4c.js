9e9475f45ef797d772f1ade2cb50dd1cefbefab1
farai <farai>
2024-11-13 12:00:08 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/(scripts)/new-script/loading-116153450f779f4c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2961,7409,7330,8875,6186,2449,3249,9628,9219,6900,1443,8705,2943,903,3146,6317,1473],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
