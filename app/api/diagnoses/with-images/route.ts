c540653927b090c11b6491f8588c283d70c0e861
farai <farai>
2024-11-14 08:07:08 +0200

    screens w/ images endpoint

---
+++ app/api/diagnoses/with-images/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { isNotNull, or } from "drizzle-orm";
+
+import { isAuthenticated } from "@/app/actions/is-authenticated";
+import logger from "@/lib/logger";
+import db from "@/databases/pg/drizzle";
+import { diagnoses as diagnosesTable } from "@/databases/pg/schema";
+import { _saveFile } from "@/databases/mutations/files";
+
+export async function GET(req: NextRequest) {
+	try {
+        // const isAuthorised = await isAuthenticated();
+
+        // if (!isAuthorised.yes) return NextResponse.json({ errors: ['Unauthorised'], });
+
+		const diagnoses = await db.query.diagnoses.findMany({
+            where: or(
+                isNotNull(diagnosesTable.image1),
+                isNotNull(diagnosesTable.image2),
+                isNotNull(diagnosesTable.image3)
+            ),
+        });
+
+		return NextResponse.json({ data: diagnoses, });
+	} catch(e: any) {
+		logger.error('[GET] /api/diagnoses/with-images', e.message);
+		return NextResponse.json({ errors: ['Internal Error'] });
+	}
+}
+
---
+++ app/api/files/[fileId]/download/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import { _getFullFileByFileId } from "@/databases/queries/files";
+import logger from "@/lib/logger";
+
+interface IParams {
+    params: {
+        fileId: string;
+    };
+}
+
+export async function GET(_: Request, { params: { fileId } }: IParams) {
+	try {
+		if (!fileId) return NextResponse.json({ errors: ['Missing fileId'] });
+
+        const { file, errors } = await _getFullFileByFileId(fileId);
+
+        if (errors?.length) return NextResponse.json({ errors });
+
+        if (!file?.data) return NextResponse.json({ errors: ["Not found"], });
+
+        return NextResponse.json({ data: file, });
+	} catch(e) {
+		logger.error('[GET] /api/files/' + fileId + '/download', e);
+		return new NextResponse('Internal Error', { status: 500, });
+	}
+}
