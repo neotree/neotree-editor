f629fdb9f3e466e43963a7ca6913b312bac87807
farai <farai>
2024-11-14 08:09:10 +0200

    prod build

--- build/static/chunks/app/(dashboard)/sessions/loading-364dbb64d8fff8fa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6186,7409,7330,8875,2449,3249,9628,9219,6900,2961,1443,8705,2943,903,3146,6317,1473],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
