d27b6ebb0b74ad37317424510361a776020c40e9
farai <farai>
2024-11-17 14:19:17 +0200

    dev

---
+++ app/api/files/upload/from-site/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import logger from "@/lib/logger";
+import { isAuthenticated } from "@/app/actions/is-authenticated";
+import { uploadFileFromSite } from "@/app/actions/files";
+
+export async function POST(req: NextRequest) {
+	try {
+        // const isAuthorised = await isAuthenticated();
+
+        // if (!isAuthorised.yes) return NextResponse.json({ errors: ['Unauthorised'], });
+
+        const params: Parameters<typeof uploadFileFromSite>[0] = await req.json();
+
+        const res = await uploadFileFromSite(params);
+
+		return NextResponse.json(res);
+	} catch(e: any) {
+		logger.error('[GET] /api/files/upload/from-site', e.message);
+		return NextResponse.json({ errors: ['Internal Error'] });
+	}
+}
