c28d15fa59a9205b47caf324ec5cc268009aa905
farai <farai>
2024-12-26 12:43:02 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/settings/sys/loading-735698f55f4b7bda.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6317,7409,8875,7330,6186,1862,2449,3249,2961,1443,8705,2943,6900,9628,9219,1473,3146,903],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
