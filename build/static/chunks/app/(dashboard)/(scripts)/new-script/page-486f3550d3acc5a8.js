cb8689d0b8ac1309d22c87224aa29bf54ce0b411
farai <farai>
2025-01-11 17:14:59 +0200

    prod build

--- build/static/chunks/app/(dashboard)/(scripts)/new-script/page-486f3550d3acc5a8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[126],{58931:function(e,n,s){Promise.resolve().then(s.bind(s,8182)),Promise.resolve().then(s.bind(s,25704)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[4868,4900,5176,9554,7776,9343,1072,6300,231,8472,1682,220,8972,8651,6942,1052,8182,7478,7023,1744],function(){return e(e.s=58931)}),_N_E=e.O()}]);
