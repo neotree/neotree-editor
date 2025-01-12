727367375ce0e58e8bea3eb3434c54f7a53f8d65
farai <farai>
2025-01-12 11:35:40 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/loading-ea07aab9f8b1d44b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7330,7409,8875,6186,1862,2961,9628,9219,2449,3249,2943,1443,8705,6900,3146,903,1473,6317],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
