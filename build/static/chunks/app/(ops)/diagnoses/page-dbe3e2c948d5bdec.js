35f27309ac796ace640722dc808f3f004a4c8ae3
farai <farai>
2025-01-13 17:57:10 +0200

    prod build

--- build/static/chunks/app/(ops)/diagnoses/page-dbe3e2c948d5bdec.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[911],{},function(n){n.O(0,[4868,4900,5176,9554,7776,9343,1072,6300,231,8472,267,6942,2997,7478,7023,1744],function(){return n(n.s=42997)}),_N_E=n.O()}]);
