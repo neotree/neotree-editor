213dc06a4d5f498b3bde72ab893d3cb367aa6e2a
farai <farai>
2025-01-21 15:53:17 +0200

    prod build

--- build/static/chunks/app/(dashboard)/settings/app/loading-b545643530463b96.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1473,7409,7330,8875,6186,1862,2961,6900,2449,3249,9628,9219,2943,1443,8705,3146,903,6317],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
