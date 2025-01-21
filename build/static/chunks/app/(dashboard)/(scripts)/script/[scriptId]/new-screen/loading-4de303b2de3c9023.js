ad5ed1c032909b5f67c0557d86ad768f096c2e40
farai <farai>
2025-01-21 16:19:37 +0200

    prod build

--- build/static/chunks/app/(dashboard)/(scripts)/script/[scriptId]/new-screen/loading-4de303b2de3c9023.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6900,7409,8875,7330,6186,1862,2961,2943,2449,3249,9628,9219,1443,8705,1473,3146,903,6317],{63521:function(){}},function(n){n.O(0,[7478,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
