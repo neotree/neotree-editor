ac80f179877a1135a9784f7198df1fae0e10915f
farai <farai>
2025-05-12 15:28:40 +0200

    prod build

--- build/types/app/api/config-keys/save/route.ts
+++ build/types/app/api/config-keys/save/route.ts
@@ -1,4 +1,4 @@
-// File: /home/morris/Documents/NEOTREE/REPOS/neotree-editor/app/api/config-keys/save/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor-master/app/api/config-keys/save/route.ts
 import * as entry from '../../../../../../app/api/config-keys/save/route.js'
 import type { NextRequest } from 'next/server.js'
 
