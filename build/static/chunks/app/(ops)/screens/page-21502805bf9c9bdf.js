ee709a0f34dd8f98b18d0996a8105208b77792ee
farai <farai>
2025-05-28 10:06:03 +0200

    prod build

---
+++ build/static/chunks/app/(ops)/screens/page-21502805bf9c9bdf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8903],{},function(n){n.O(0,[4868,4900,4489,9554,7776,9343,1072,6300,231,8472,5149,6942,2997,7478,7023,1744],function(){return n(n.s=42997)}),_N_E=n.O()}]);
