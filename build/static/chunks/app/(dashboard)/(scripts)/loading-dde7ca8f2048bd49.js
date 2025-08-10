093f957644b8b09529662d2f559aacd9a5db5566
farai <farai>
2025-08-10 16:15:18 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/(scripts)/loading-dde7ca8f2048bd49.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4205,8502,9424,8832,1411],{89595:function(n,e,u){Promise.resolve().then(u.bind(u,76942))}},function(n){n.O(0,[4868,1086,7468,6044,9527,193,7776,625,1565,6942,7478,7023,1744],function(){return n(n.s=89595)}),_N_E=n.O()}]);
