0852b0dc18d32f50a9bd634d458f1ed47d25e197
farai <farai>
2025-08-24 14:16:07 +0200

    prod build

---
+++ build/static/chunks/app/(dashboard)/hospitals/loading-19cf3a26a90325ed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8832,9424,8502,1411,4205],{89595:function(n,e,u){Promise.resolve().then(u.bind(u,76942))}},function(n){n.O(0,[4868,1086,7468,6044,9527,193,7776,625,1565,6942,7478,7023,1744],function(){return n(n.s=89595)}),_N_E=n.O()}]);
