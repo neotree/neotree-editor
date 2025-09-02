4b258205ddca4b809175d4d4acd4f3cce4189d94
farai <farai>
2025-09-02 18:56:45 +0200

    datakeys

--- scripts/data-keys-mwi.ts
+++ scripts/data-keys-mwi.ts
@@ -1,11 +1,11 @@
 import '@/server/env';
-import { _extractDataKeys } from "@/databases/mutations/data-keys";
+import { _remoteExtractDataKeys } from "@/databases/mutations/data-keys";
 
 main();
 
 async function main() {
     try {
-        const res = await _extractDataKeys({ country: 'mwi', });
+        const res = await _remoteExtractDataKeys({ country: 'mwi', });
         console.log('SUCCESS');
         console.log(res);
     } catch(e: any) {
