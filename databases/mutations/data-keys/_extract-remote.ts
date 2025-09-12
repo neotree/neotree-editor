d86a5539049fa26fd6fff1a9f5e4da778e3de255
farai <farai>
2025-09-12 09:34:46 +0200

    datakeys

--- databases/mutations/data-keys/_extract-remote.ts
+++ databases/mutations/data-keys/_extract-remote.ts
@@ -5,10 +5,10 @@ import queryString from "query-string";
 import logger from "@/lib/logger";
 import db from "@/databases/pg/drizzle";
 import * as schema from "@/databases/pg/schema";
-import { _saveDataKeys } from "./_save";
 import { getScriptsWithItems } from "@/app/actions/scripts";
 import { getSiteAxiosClient } from "@/lib/server/axios";
-
+import { _saveDataKeys } from "./_save";
+import { scrapDataKeys } from "./_extract";
 type DataKey = typeof schema.dataKeys.$inferInsert;
 
 export type RemoteExtractDataKeysResponse = {
@@ -85,158 +85,10 @@ export async function _remoteExtractDataKeys(opts: RemoteExtractDataKeysParams):
         const drugsLibrary = await db.query.drugsLibrary.findMany();
         console.log('Fetching drugsLibrary... DONE');
 
-        let extractedKeys: DataKey[] = [];
-
-        console.log('Extacting screens keys...');
-        screens.forEach(s => {
-            if (s.key) {
-                const dataKey: DataKey = { 
-                    id: undefined,
-                    uuid: v4(),
-                    name: s.key, 
-                    label: s.label || '', 
-                    dataType: null, 
-                    parentKeys: [], 
-                    version: 1,
-                };
-                switch(s.type) {
-                    case 'timer':
-                        dataKey.dataType = 'timer';
-                        break;
-                    case 'yesno':
-                        dataKey.dataType = 'yesno';
-                        break;
-                    case 'single_select':
-                        dataKey.dataType = 'single_select';
-                        break;
-                    case 'multi_select':
-                        dataKey.dataType = 'multi_select';
-                        break;
-                    default:
-                        //
-                }
-
-                extractedKeys.push(dataKey);
-            }
-
-            s.items.forEach(item => {
-                const key = item.id || item.key;
-                if (key) {
-                    const dataKey: DataKey = {
-                        id: undefined,
-                        uuid: v4(),
-                        name: key,
-                        label: item.label || '',
-                        dataType: null,
-                        parentKeys: !s.key ? [] : [s.key],
-                        version: 1,
-                        defaults: { 
-                            dataType: item.subType || undefined, 
-                            severity_order: item.severity_order || undefined, 
-                            score: item.score || undefined, 
-                        },
-                    };
-
-                    switch(s.type) {
-                        case 'single_select':
-                            dataKey.dataType = 'single_select_option';
-                            break;
-                        case 'multi_select':
-                            dataKey.dataType = 'multi_select_option';
-                            break;
-                        case 'diagnosis':
-                            dataKey.dataType = 'diagnosis';
-                            break;
-                        case 'checklist':
-                            dataKey.name = item.key || item.id;
-                            dataKey.dataType = 'checklist_option';
-                            break;
-                        case 'zw_edliz_summary_table':
-                            dataKey.dataType = 'zw_edliz_summary_table_option';
-                            break;
-                        case 'mwi_edliz_summary_table':
-                            dataKey.dataType = 'mwi_edliz_summary_table_option';
-                            break;
-                        case 'edliz_summary_table':
-                            dataKey.dataType = 'edliz_summary_table_option';
-                            break;
-                        default:
-                            //
-                    }
-
-                    extractedKeys.push(dataKey);
-                }
-            });
-
-            s.fields.forEach((f, i) => {
-                const dataKey: DataKey = {
-                    id: undefined,
-                    uuid: v4(),
-                    name: f.key,
-                    label: f.label || '',
-                    dataType: f.type,
-                    parentKeys: !s.key ? [] : [s.key],
-                    version: 1,
-                    defaults: { dataType: f.dataType || undefined, },
-                };
-                extractedKeys.push(dataKey);
-
-                switch(f.type) {
-                    case 'dropdown':
-                        (f.values || '').split('\n')
-                            .map((v = '') => v.trim())
-                            .filter((v: any) => v)
-                            .forEach((v: any) => {
-                                v = v.split(',');
-                                extractedKeys.push({ 
-                                    uuid: v4(),
-                                    id: undefined,
-                                    name: v[0], 
-                                    label: v[1], 
-                                    dataType: 'dropdown_option', 
-                                    parentKeys: !f.key ? [] : [f.key],
-                                    version: 1,
-                                });
-                            });
-                        break;
-                    default:
-                        //
-                }
-            });
-        });
-
-        console.log('Extacting diagnoses keys...');
-        diagnoses.forEach(d => {
-            if (d.key) {
-                extractedKeys.push({ 
-                    id: undefined,
-                    uuid: v4(),
-                    name: d.key, 
-                    label: d.name, 
-                    dataType: 'diagnosis', 
-                    parentKeys: [],
-                    version: 1,
-                });
-            }
-            
-            // d.symptoms.forEach(s => {
-            //     if (s.name) data.push(s.name);
-            // });
-        });
-
-        console.log('Extacting drugsLibrary keys...');
-        drugsLibrary.forEach(d => {
-            if (d.key) {
-                extractedKeys.push({
-                    id: undefined,
-                    uuid: v4(), 
-                    name: d.key, 
-                    label: d.drug, 
-                    dataType: d.type, 
-                    parentKeys: [],
-                    version: 1,
-                });
-            }
+        let extractedKeys: DataKey[] = scrapDataKeys({
+            screens,
+            diagnoses,
+            drugsLibrary,
         });
 
         console.log(`Processing extracted keys (${extractedKeys.length}) ...`);
