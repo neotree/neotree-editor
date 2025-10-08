7936c65e16b6b5065979ca427dfa47400a8315db
farai <farai>
2025-10-08 13:12:08 +0200

    datakeys

--- lib/data-keys.ts
+++ lib/data-keys.ts
@@ -3,7 +3,7 @@ import { v4 as uuidV4 } from "uuid";
 import { _getScreens, _getDiagnoses } from "@/databases/queries/scripts";
 import { _getDrugsLibraryItems } from "@/databases/queries/drugs-library";
 import { _getDataKeys, DataKey } from "@/databases/queries/data-keys";
-import { diagnoses } from "@/databases/pg/schema";
+import { diagnoses, drugsLibrary } from "@/databases/pg/schema";
 
 type KeyWithoutOptions = {
     name: string;
@@ -174,6 +174,174 @@ export async function linkScrappedKeysToDataKeys({ scrappedKeys, importedDataKey
             options,
         };
     });
+}
+
+export async function parseImportedDataKeys({ 
+    importedScrappedKeys = [], 
+    importedDataKeys = [], 
+    importedScreens = [],
+    importedDiagnoses = [],
+    importedDrugsLibraryItems = [],
+}: {
+    importedDataKeys: DataKey[];
+    importedScrappedKeys: Awaited<ReturnType<typeof scrapDataKeys>>;
+    importedScreens?: Awaited<ReturnType<typeof _getScreens>>['data'];
+    importedDiagnoses?: Awaited<ReturnType<typeof _getDiagnoses>>['data'];
+    importedDrugsLibraryItems?: Awaited<ReturnType<typeof _getDrugsLibraryItems>>['data'];
+}) {
+    let { data: localDataKeys, } = await _getDataKeys();
+
+    const importedUniqueIdDataKeyMap: Record<string, DataKey> = {};
+    const importedKeyJsonDataKeyMap: Record<string, DataKey> = {};
+    const importedKeyJsonNewIdsMap: Record<string, {
+        id?: number;
+        uuid: string;
+        uniqueKey: string;
+    }> = {};
+    importedDataKeys.forEach(k => {
+        const localDataKey = pickDataKey(localDataKeys, k) as undefined | DataKey;
+        importedUniqueIdDataKeyMap[k.uniqueKey] = k;
+        importedKeyJsonDataKeyMap[dataKeyToJSON(k)] = k;
+        importedKeyJsonNewIdsMap[dataKeyToJSON(k)] = {
+            id: localDataKey?.id,
+            uniqueKey: localDataKey?.uniqueKey || uuidV4(),
+            uuid: localDataKey?.uuid || uuidV4(),
+        };
+    });
+
+    const localUniqueIdDataKeyMap: Record<string, DataKey> = {};
+    const localKeyJsonDataKeyMap: Record<string, DataKey> = {};
+    localDataKeys.forEach(k => {
+        localUniqueIdDataKeyMap[k.uniqueKey] = k;
+        localKeyJsonDataKeyMap[dataKeyToJSON(k)] = k;
+    });
+
+    localDataKeys = localDataKeys.map(k => ({
+        ...k,
+        options: k.options.filter(o => importedUniqueIdDataKeyMap[o]),
+    }));
+
+    let parsed = importedDataKeys.map(k => {
+        const key = importedKeyJsonNewIdsMap[dataKeyToJSON(k)];
+        return {
+            ...k,
+            id: key.id,
+            uniqueKey: key.uniqueKey,
+            uuid: key.uuid,
+            isDifferentFromLocal: false,
+            canSave: false,
+            options: k.options.filter(o => importedUniqueIdDataKeyMap[o]).map(o => {
+                const key = importedUniqueIdDataKeyMap[o];
+                return importedKeyJsonNewIdsMap[dataKeyToJSON(key)].uniqueKey;
+            }),
+        };
+    });
+
+    parsed = parsed.map(k => {
+        const localDataKey = pickDataKey(localDataKeys, k) as undefined | DataKey;
+
+        const isDifferentFromLocal = !localDataKey || (
+            localDataKey &&
+            (
+                (dataKeyToJSON(localDataKey) !== dataKeyToJSON(k)) ||
+                (localDataKey.options.length !== k.options.length) ||
+                !!localDataKey.options.find((o, i) => k.options.indexOf(o) !== i)
+            )
+        );
+
+        let isScrapped = !!importedScrappedKeys.find(sk => dataKeyToJSON(sk) === dataKeyToJSON(k));
+
+        return {
+            ...k,
+            isDifferentFromLocal,
+            canSave: isScrapped && isDifferentFromLocal,
+        };
+    });
+
+    return {
+        dataKeys: parsed,
+
+        drugsLibrary: importedDrugsLibraryItems,
+
+        diagnoses: importedDiagnoses.map(s => {
+            const name = s.key || s.name;
+            const k = {
+                label: s.name,
+                name,
+                dataType: 'diagnosis',
+            }; 
+            const keyId = pickDataKey(parsed, k)?.uniqueKey;
+            return {
+                ...s,
+                keyId,
+                symptoms: (s.symptoms || []).map(f => {
+                    const name = f.key || f.name;
+                    const k = {
+                        label: f.name,
+                        name,
+                        dataType: `diagnosis_symptom_${f.type}`,
+                    };
+                    const keyId = pickDataKey(parsed, k)?.uniqueKey;
+                    return {
+                        ...f,
+                        keyId,
+                    }
+                }),
+            };
+        }),
+
+        screens: importedScreens.map(s => {
+            const k = {
+                label: s.label,
+                name: s.key,
+                dataType: s.type,
+            }; 
+            const keyId = pickDataKey(parsed, k)?.uniqueKey || '';
+            return {
+                ...s,
+                keyId,
+                fields: (s.fields || []).map(f => {
+                    const dataType = f.type;
+                    const k = {
+                        label: f.label,
+                        name: f.key,
+                        dataType,
+                    };
+                    const keyId = pickDataKey(parsed, k)?.uniqueKey;
+                    return {
+                        ...f,
+                        keyId,
+                        items: (f.items || []).map(item => {
+                            const k = {
+                                label: item.label as string,
+                                name: item.value as string,
+                                dataType: `${dataType}_option`,
+                            };
+                            const keyId = pickDataKey(parsed, k)?.uniqueKey;
+                            return {
+                                ...f,
+                                keyId,
+                            };
+                        }),
+                    };
+                }),
+                items: (s.items || []).map(f => {
+                    let dataType = `${s.type}_option`;
+                    if (s.type === 'diagnosis') dataType = 'diagnosis';
+                    const k = {
+                        label: f.label,
+                        name: f.key,
+                        dataType,
+                    };
+                    const keyId = pickDataKey(parsed, k)?.uniqueKey;
+                    return {
+                        ...f,
+                        keyId,
+                    };
+                }),
+            };
+        }),
+    };
 }
 
 export function mergeScrappedKeys(...scrappedKeys: Scrapped[][]): KeyWithOptions[] {
