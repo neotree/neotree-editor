f8d1af58f696e32151141d7dc080b4f16709c5c6
farai <farai>
2025-10-09 07:38:24 +0200

    datakeys ctx - DataType

--- contexts/data-keys/index.tsx
+++ contexts/data-keys/index.tsx
@@ -19,6 +19,7 @@ import { dataKeysSortOpts } from "@/constants";
 import * as actions from '@/app/actions/data-keys';
 import { DeleteDataKeysParams, DeleteDataKeysResponse, SaveDataKeysParams } from '@/databases/mutations/data-keys';
 import { Pagination } from "@/types";
+import { _getDataKeys } from "@/databases/queries/data-keys";
 
 
 function paginateData<T>(
@@ -42,22 +43,7 @@ function paginateData<T>(
     };
 }
 
-export type DataKey = {
-    uuid: string;
-    uniqueKey: string;
-    name: string;
-    refId: string;
-    dataType: string;
-    label: string;
-    options: any[];
-    metadata: any;
-    version: string;
-    createdAt: string;
-    updatedAt: string;
-    deletedAt: string | null;
-    isDraft: boolean;
-    isDeleted: boolean;
-};
+export type DataKey = Awaited<ReturnType<typeof _getDataKeys>>['data'][0];
 
 export type DataKeyFormData = {
     name: DataKey['name'];
