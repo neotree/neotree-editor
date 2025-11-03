f54ec6ada237bd306f7ed159652694e7bc85e792
farai <farai>
2025-11-03 11:06:30 +0200

    prod build

--- build/types/app/(dashboard)/settings/app/page.ts
+++ build/types/app/(dashboard)/settings/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/(dashboard)/settings/app/page.tsx
+// File: /home/farai/Workbench/Neotree/neotree-editor-master/app/(dashboard)/settings/app/page.tsx
 import * as entry from '../../../../../../app/(dashboard)/settings/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
