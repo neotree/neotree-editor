f550ef911f6fe961ed0a22b3f3ca3400ab81c7c3
KumaloWilson <kumalowilson900@gmail.com>
2025-11-06 16:18:02 +0200

    deploying change logs on demo server for testing

--- build/types/app/api/app/device/[deviceId]/route.ts
+++ build/types/app/api/app/device/[deviceId]/route.ts
@@ -1,4 +1,4 @@
-// File: /home/farai/Workbench/Neotree/neotree-editor-master/app/api/app/device/[deviceId]/route.ts
+// File: /home/watkay/ReactProjects/neotree-editor/app/api/app/device/[deviceId]/route.ts
 import * as entry from '../../../../../../../app/api/app/device/[deviceId]/route.js'
 import type { NextRequest } from 'next/server.js'
 
