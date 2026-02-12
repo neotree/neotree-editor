cfa8c80b3178b83aaaec5f835ac2ff1676f6b0cb
farai <farai.matare@bwscloud.tech>
2026-02-12 14:00:07 +0200

    prod build

--- build/static/chunks/app/(ops)/diagnoses/page-90623d05f8d17de9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[911],{},function(n){n.O(0,[6137,2696,3186,9327,5972,3464,7790,2972,1043,5262,2686,6024,4465,7478,2117,1744],function(){return n(n.s=74465)}),_N_E=n.O()}]);
