0e2a35151aac281e029cbdd16b1d39ab1e6d2b3a
KumaloWilson <kumalowilson900@gmail.com>
2026-02-16 12:41:14 +0200

    prod demo, master merge

--- build/types/app/api/files/get-image-size/route.ts
+++ build/types/app/api/files/get-image-size/route.ts
@@ -1,4 +1,4 @@
-// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/files/get-image-size/route.ts
+// File: /home/watkay/ReactProjects/neotree-editor/app/api/files/get-image-size/route.ts
 import * as entry from '../../../../../../app/api/files/get-image-size/route.js'
 import type { NextRequest } from 'next/server.js'
 
