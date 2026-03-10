3ff23b633cb76160c8b0884f79f123c2ac0e43e3
KumaloWilson <kumalowilson900@gmail.com>
2026-03-10 14:08:29 +0200

    datakey confidentiality deployment

---
+++ build/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/watkay/ReactProjects/neotree-editor/app/layout":["static/media/1e41be92c43b3255-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
