bb46c4b921f84e1d79db3f3c10fb0d539eb04683
KumaloWilson <kumalowilson900@gmail.com>
2026-03-16 12:07:37 +0200

    scripts metadata screen enhancement

--- build/types/app/api/drugs-library/remove-references/route.ts
+++ build/types/app/api/drugs-library/remove-references/route.ts
@@ -1,4 +1,4 @@
-// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/drugs-library/remove-references/route.ts
+// File: /home/watkay/ReactProjects/neotree-editor/app/api/drugs-library/remove-references/route.ts
 import * as entry from '../../../../../../app/api/drugs-library/remove-references/route.js'
 import type { NextRequest } from 'next/server.js'
 
