ffb7565dbc482d2af159bd12773e166254f66753
KumaloWilson <kumalowilson900@gmail.com>
2026-03-17 09:30:47 +0200

    datkey update bug fix

--- build/types/app/api/changelogs/stats/route.ts
+++ build/types/app/api/changelogs/stats/route.ts
@@ -1,4 +1,4 @@
-// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/changelogs/stats/route.ts
+// File: /home/watkay/ReactProjects/neotree-editor/app/api/changelogs/stats/route.ts
 import * as entry from '../../../../../../app/api/changelogs/stats/route.js'
 import type { NextRequest } from 'next/server.js'
 
