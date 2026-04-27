f69433535d0971e61ef0bfa12f2e180ada3c6e34
KumaloWilson <kumalowilson900@gmail.com>
2026-04-27 11:22:30 +0200

    added source

--- databases/pg/migrations/0013_data_key_sync_draft_origin.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "nt_screens_drafts"
-ADD COLUMN "draft_origin" text;
-
-ALTER TABLE "nt_diagnoses_drafts"
-ADD COLUMN "draft_origin" text;
-
-ALTER TABLE "nt_problems_drafts"
-ADD COLUMN "draft_origin" text;
