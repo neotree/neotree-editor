5839d9ef3f21a9ba36be23fc4956f4b866c7dd31
farai <farai.matare@bwscloud.tech>
2026-05-11 11:23:12 +0200

    prod build

--- build/types/app/api/users/is-email-registered/route.ts
+++ build/types/app/api/users/is-email-registered/route.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/api/users/is-email-registered/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/users/is-email-registered/route.ts
 import * as entry from '../../../../../../app/api/users/is-email-registered/route.js'
 import type { NextRequest } from 'next/server.js'
 
