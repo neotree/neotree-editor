3aa03a366f0dae6cf04eb42ae85ac119dcad700f
farai <farai.matare@bwscloud.tech>
2026-05-19 10:35:35 +0200

    prod build

--- build/types/app/api/save/route.ts
+++ build/types/app/api/save/route.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/api/save/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/save/route.ts
 import * as entry from '../../../../../app/api/save/route.js'
 import type { NextRequest } from 'next/server.js'
 
