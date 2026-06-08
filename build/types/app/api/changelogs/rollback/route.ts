39c830ba0aa99a0892f0ca159b01d2c4a2bff7e9
farai <farai.matare@bwscloud.tech>
2026-06-08 08:05:44 +0200

    prod build

--- build/types/app/api/changelogs/rollback/route.ts
+++ build/types/app/api/changelogs/rollback/route.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/api/changelogs/rollback/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/changelogs/rollback/route.ts
 import * as entry from '../../../../../../app/api/changelogs/rollback/route.js'
 import type { NextRequest } from 'next/server.js'
 
