dbb723f1e950c79af746a6491cfcd36077c64de2
farai <farai.matare@bwscloud.tech>
2026-07-07 12:43:15 +0200

    Conditional expression playground

---
+++ app/(ops)/conditional-exp/_eval.ts
@@ -0,0 +1,286 @@
+export type ScreenEntryValue = {
+  value?: any;
+  value2?: any;
+  key2?: any;
+  valueText?: any;
+  valueLabel?: any;
+  unit?: string;
+  label?: string;
+  key?: string;
+  parentKey?: string;
+  inputKey?: string;
+  type?: string;
+  dataType?: string;
+  confidential?: boolean;
+  exportValue?: any;
+  exportLabel?: any;
+  exportType?: any;
+  calculateValue?: any;
+  exclusive?: any;
+  error?: any;
+  diagnosis?: any;
+  problem?: any;
+  prePopulate?: any[];
+  printable?: boolean;
+  printDisplayColumns?: 1 | 2;
+  extraLabels?: string[] | {
+    title?: string;
+    label: string;
+    printTitle?: boolean;
+  }[];
+  selected?: boolean;
+  data?: any;
+  comments?: { key?: string; label: string; }[];
+  listStyle?: 'none' | 'number' | 'bullet';
+  score?: number;
+};
+
+export type ScreenEntry = {
+  value?: ScreenEntryValue[];
+  values: ScreenEntryValue[];
+  screen: {
+    type: string;
+  }; 
+};
+
+export function evaluateCondition(condition: string, defaultEval = false) {
+    let conditionMet = defaultEval;
+    try {
+        conditionMet = eval(condition);
+    } catch (e) {
+        // do nothing
+    }
+    return conditionMet;
+}
+
+export function sanitizeCondition(condition: string) {
+    let sanitized = condition
+        .replace(new RegExp(' and ', 'gi'), ' && ')
+        .replace(new RegExp(' or ', 'gi'), ' || ')
+        .replace(new RegExp(' = ', 'gi'), ' == ');
+    sanitized = sanitized.split(' ')
+        .map(s => s[0] === '$' ? `'${s}'` : s).join(' ');
+    return sanitized;
+}
+
+export function parseConditionString(condition: string, _key = '', value: any) {
+    const s = (condition || '').toLowerCase().split('$').join(' $');
+    const key = (_key || '').toLowerCase();
+    const parsed = s.replace(/\s\s+/g, ' ')
+        .split(`$${key} =`).join(`${value} =`)
+        .split(`$${key}=`).join(`${value} =`)
+        .split(`$${key} >`).join(`${value} >`)
+        .split(`$${key}>`).join(`${value} >`)
+        .split(`$${key} <`).join(`${value} <`)
+        .split(`$${key}<`).join(`${value} <`)
+        .split(`$${key}!`).join(`${value} !`)
+        .split(`$${key} !`).join(`${value} !`);
+    return parsed;
+}
+
+const configuration: Record<string, any> = {};
+
+export function parseCondition(
+    _condition = '', 
+    entries: ScreenEntry[] = []
+) {
+    _condition = `${_condition || ''}`.split('\n').map(_condition => {
+        const _form = [...entries];
+
+        _condition = _condition.replace(/\[(.*?)\]/gi, (_, match: string) => {
+            return parseCondition(match, _form);
+        });
+
+        if (
+            _condition.match(/ excludes /gi) ||
+            _condition.match(/ includes /gi)
+        ) {
+            const [key, vals] = _condition.match(/ excludes /gi) ?
+                _condition.split(/ excludes /gi).map(s => s.trim())
+                :
+                _condition.split(/ includes /gi).map(s => s.trim());
+
+            const entryVals = _form.map(e => {
+                let found: string[] = [];
+                const entryVals = e.value || e.values || [];
+                entryVals.forEach(v => {
+                    if (`$${v.key?.toLowerCase?.()}` === key.toLowerCase()) {
+                        const val = Array.isArray(v.value) ? v.value : [v.value];
+                        val.forEach(v => found.push(v.key));
+                    }
+                });
+                return found;
+            }).reduce((acc, arr) => [...acc, ...arr], []);
+
+            _condition = `${vals || ''}`
+                .replace(/\((.*?)\)/, '$1')
+                .split(',')
+                .map(v => v.trim().replaceAll('"', '').replaceAll("'", '').replaceAll('`', '').replaceAll('`', ''))
+                .map(v => {
+                    let includes = entryVals.map(v => v.toLowerCase()).includes(v.toLowerCase());
+                    if (_condition.match(/ excludes /gi)) {
+                        includes = !includes;
+                    }
+                    return includes;
+                })
+                .join(' or ');
+
+            return _condition;
+        }
+
+        const parseValue = (condition = '', { value, calculateValue, type, inputKey, key, dataType }: ScreenEntryValue) => {
+            value = ((calculateValue === null) || (calculateValue === undefined)) ? value : calculateValue;
+            value = ((value === null) || (value === undefined)) ? 'no value' : value;
+            const t = dataType || type;
+    
+            switch (t) {
+                case 'boolean':
+                    value = value === 'false' ? false : Boolean(value);
+                    break;
+                default:
+                    if(key==='createdAt'){
+                        value=value
+                    }else{
+                        value = JSON.stringify(value)
+                    }
+            }
+    
+            return parseConditionString(condition, inputKey || key, value);
+        };
+    
+        let parsedCondition = _form.reduce((condition: string, { screen, values, value }: ScreenEntry) => {
+            values = value || values || [];
+
+            values = values.reduce((acc: typeof values, v) => {
+                acc = [...acc, v];
+                if (v.value2 && v.key2) acc = [...acc, { value: v.value2, key: v.key2, }];
+                return acc;
+            }, []);
+            
+            // First filter out null/undefined values
+            values = values.filter(e => (e.value !== null) && (e.value !== undefined));
+            
+            // Handle both array and non-array values
+            values = values
+                .reduce((acc: ScreenEntryValue[], e) => {
+                    acc = [
+                        ...acc,
+                        ...(e.value && Array.isArray(e.value) ? e.value : [e]),
+                    ];
+
+                    acc.forEach(v => {
+                        if (v.value2) {
+                            acc.push({
+                                ...v,
+                                value: v.value2,
+                            });
+                        }
+                    });
+
+                    return acc;
+                }, []);
+    
+            let c = values.reduce((acc, v) => parseValue(acc, v), condition);
+
+            let chunks: string[] = values.filter(v => v.parentKey)
+                .map(v => parseValue(condition, {
+                    ...v,
+                    key: v.parentKey,
+                }))
+                .filter(c => c !== condition);
+    
+            if (screen) {
+                switch (screen.type) {
+                    case 'multi_select':
+                        chunks = values.map(v => parseValue(condition, v)).filter(c => c !== condition);
+                        break;
+                    default:
+                    // do nothing
+                }
+            }
+
+            if (chunks.length) {
+                c = chunks.map(c => `(${c})`).join(' || ');
+            }
+    
+            return c || condition;
+        }, _condition);
+    
+        if (configuration) {
+            parsedCondition = Object.keys(configuration).reduce((acc, key) => {
+                return parseConditionString(acc, key, configuration[key] ? true : false);
+            }, parsedCondition);
+        }
+    
+        return `(${sanitizeCondition(parsedCondition)})`;
+    }).join(' && ');
+
+    return _condition;
+}
+
+export const screenTypes = [
+    { type: 'fluids', },
+    { type: 'drugs', },
+    { type: 'yesno', },
+    { type: 'checklist', },
+    { type: 'multi_select', },
+    { type: 'single_select', },
+    { type: 'form', },
+    { type: 'timer', },
+    { type: 'progress', },
+    { type: 'management', },
+    { type: 'list', },
+    { type: 'diagnosis', },
+    { type: 'problems', },
+    { type: 'zw_edliz_summary_table', },
+    { type: 'mwi_edliz_summary_table', },
+    { type: 'edliz_summary_table', },
+];
+
+export const entryTypes = [
+    {
+        type: 'boolean',
+    },
+    {
+        type: 'date',
+    },
+    {
+        type: 'dropdown',
+    },
+    {
+        type: 'multi_select',
+    },
+    {
+        type: 'text',
+    },
+    {
+        type: 'time',
+    },
+    {
+        type: 'diagnosis',
+    },
+    {
+        type: 'problem',
+    },
+    {
+        type: 'drug',
+    },
+    {
+        type: 'fluid',
+    },
+    {
+        type: 'checklist',
+    },
+    {
+        type: 'multi_select',
+    },
+    {
+        type: 'single_select',
+    },
+    {
+        type: 'timer',
+    },
+    {
+        type: 'yesno',
+    },
+];
---
+++ app/(ops)/conditional-exp/page.tsx
@@ -0,0 +1,406 @@
+'use client';
+
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import CodeEditor, { TextareaCodeEditorProps } from '@uiw/react-textarea-code-editor';
+
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger, } from '@/components/ui/collapsible';
+import { 
+    Dialog, 
+    DialogClose, 
+    DialogContent, 
+    DialogDescription, 
+    DialogFooter, 
+    DialogHeader, 
+    DialogTitle, 
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+    type ScreenEntry,
+    type ScreenEntryValue,
+    entryTypes,
+    screenTypes,
+    evaluateCondition,
+    parseCondition,
+} from './_eval';
+import { ChevronDown, EditIcon, TrashIcon } from 'lucide-react';
+
+export default function ConditionalExp() {
+    const [condition, setCondition] = useState('');
+    const [entries, setEntries] = useState<ScreenEntry[]>([]);
+
+    const setEntry = useCallback((
+        entry: ScreenEntry, 
+        entryIndex: number,
+    ) => {
+        alert(entryIndex)
+        setEntries(prev => {
+            if (!prev.length) return [entry];
+            return prev.map((e, i) => ({
+                ...(i === entryIndex ? { ...e, ...entry, } : e),
+            }));            
+        });
+    }, []);
+
+    const canTest = entries.length;
+
+    return (
+        <>
+            <div className="h-full w-full fixed flex flex-col overflow-y-auto">
+                <div className="my-auto mx-auto w-[90%] max-w-[800px] py-10 flex flex-col gap-y-4">
+                    {!entries.length ? null : (
+                        <Collapsible>
+                            <CollapsibleTrigger asChild>
+                                <Button variant="ghost">Show entries <ChevronDown className="w-4 h-4" /></Button>
+                            </CollapsibleTrigger>
+
+                            <CollapsibleContent>
+                                <div className="flex flex-col gap-y-2 mt-4">
+                                    {entries.map((e, i) => {
+                                        return (
+                                            <Card key={i}>
+                                                <CardContent>
+                                                    <CardHeader>
+                                                        <div className="flex justify-end">
+                                                            <JsonModal 
+                                                                entry={e}
+                                                                entryIndex={i}
+                                                                onSave={e => setEntries(prev => prev.map((entr, j) => ({
+                                                                    ...(i !== j ? entr : {
+                                                                        // ...entr,
+                                                                        ...e,
+                                                                    }),
+                                                                })))}
+                                                            />
+                                                            <Button
+                                                                variant="ghost"
+                                                                onClick={() => {
+                                                                    const confirmed = confirm('Are you sure?');
+                                                                    if (confirmed) {
+                                                                        setEntries(prev => prev.filter((_, j) => i !== j));
+                                                                    }
+                                                                }}
+                                                            >
+                                                                <TrashIcon className="w-4 h-4" />
+                                                            </Button>
+                                                        </div>
+                                                    </CardHeader>
+
+                                                    <pre>
+                                                        {JSON.stringify(e, null, 4)}
+                                                    </pre>
+                                                </CardContent>
+                                            </Card>
+                                        )
+                                    })}
+                                </div>
+                            </CollapsibleContent>
+                        </Collapsible>
+                    )}
+
+                    <div>
+                        <Textarea 
+                            value={condition}
+                            placeholder="Condition"
+                            rows={4}
+                            disabled={!canTest}
+                            onChange={e => setCondition(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex flex-row gap-x-4 justify-end">
+                        <Modal
+                            entryIndex={entries.length}
+                            onSave={(entry) => setEntries(prev => [...prev, entry])}
+                        />
+
+                        <Button
+                            disabled={!canTest}
+                            onClick={() => {
+                                const res = evaluateCondition(parseCondition(condition, entries));
+                                alert(parseCondition(condition, entries) + ' ' + res);
+                            }}
+                        >Test</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function Modal({
+    entry,
+    entryIndex,
+    trigger,
+    onSave,
+}: {
+    entry?: ScreenEntry;
+    entryIndex: number;
+    trigger?: React.ReactNode;
+    onSave: (entry: ScreenEntry, entryIndex: number) => void;
+}) {
+    const [entryValue, setEntryValue] = useState<ScreenEntryValue>({
+        type: '',
+        key: '',
+        value: '',
+    });
+    const [entryValueIndex, setEntryValueIndex] = useState(0);
+    const [form, setForm] = useState<ScreenEntry>(entry || {
+        values: [],
+        screen: {
+            type: '',
+        },
+    });
+
+    const save = useCallback(() => {
+        onSave(form, entryIndex);
+    }, [form, entryIndex, onSave]);
+
+    const canSave = !!form.screen.type && !!(
+        form.value?.length ||
+        form.values?.length
+    );
+
+    useEffect(() => {
+        if (
+            entryValue.key &&
+            entryValue.value &&
+            entryValue.type &&
+            form.screen.type
+        ) {
+            setForm(prev => ({
+                ...prev,
+                values: !form.values.length ? [entryValue] : prev.values.map((v, i) => {
+                    if (i === entryValueIndex) {
+                        return {
+                            ...v,
+                            ...entryValue,
+                        };
+                    }
+                    return v;
+                }),
+            }));
+        } else {
+            setForm(prev => ({
+                ...prev,
+                values: prev.values.filter((_, i) => i !== entryValueIndex),
+            }));
+        }
+    }, [entryValue, entryValueIndex, form.screen.type]);
+
+    return (
+        <>
+            <Dialog>
+                {trigger || (
+                    <DialogTrigger asChild>
+                        <Button
+                            variant="ghost"
+                        >Add value</Button>
+                    </DialogTrigger>
+                )}
+                
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{''}</DialogTitle>
+                        <DialogDescription>{''}</DialogDescription>
+                    </DialogHeader>
+
+                    <div className="flex flex-col gap-y-4">
+                        <div>
+                            <Label>Screen type</Label>
+                            <Select
+                                value={form.screen.type}
+                                onValueChange={type => setForm(prev => ({
+                                    ...prev,
+                                    screen: {
+                                        ...prev.screen,
+                                        type,
+                                    },
+                                }))}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue />
+                                </SelectTrigger>
+
+                                <SelectContent>
+                                    {screenTypes.map((t, i) => {
+                                        const key = `${i}`;
+                                        return (
+                                            <SelectItem
+                                                key={key}
+                                                value={t.type}
+                                            >
+                                                {t.type}
+                                            </SelectItem>
+                                        );
+                                    })}
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div>
+                            <Label>Entry type</Label>
+                            <Select
+                                value={entryValue.type}
+                                onValueChange={type => setEntryValue(prev => ({
+                                    ...prev,
+                                    type,
+                                }))}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue />
+                                </SelectTrigger>
+
+                                <SelectContent>
+                                    {entryTypes.map((t, i) => {
+                                        const key = `${i}`;
+                                        return (
+                                            <SelectItem
+                                                key={key}
+                                                value={t.type}
+                                            >
+                                                {t.type}
+                                            </SelectItem>
+                                        );
+                                    })}
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div>
+                            <Label>Key</Label>
+                            <Input 
+                                value={entryValue.key}
+                                onChange={e => setEntryValue(prev => ({
+                                    ...prev,
+                                    key: e.target.value,
+                                }))}
+                            />
+                        </div>
+
+                        <div>
+                            <Label>Value</Label>
+                            <Input 
+                                value={entryValue.value}
+                                onChange={e => setEntryValue(prev => ({
+                                    ...prev,
+                                    value: e.target.value,
+                                }))}
+                            />
+                        </div>
+                    </div>
+                    
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button
+                                variant="ghost"
+                            >Cancel</Button>
+                        </DialogClose>
+
+                        <DialogClose asChild>
+                            <Button
+                                disabled={!canSave}
+                                onClick={() => save()}
+                            >Save</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+}
+
+function JsonModal({
+    entry,
+    entryIndex,
+    trigger,
+    onSave,
+}: {
+    entry?: ScreenEntry;
+    entryIndex: number;
+    trigger?: React.ReactNode;
+    onSave: (entry: ScreenEntry, entryIndex: number) => void;
+}) {
+    const [form, setForm] = useState(JSON.stringify({
+        values: [],
+        screen: {
+            type: '',
+        },
+        ...entry
+    }, null, 4));
+
+    const save = useCallback(() => {
+        onSave(JSON.parse(form), entryIndex);
+    }, [form, entryIndex, onSave]);
+
+    const isJsonValid = useMemo(() => {
+        return (() => {
+            try {
+                JSON.parse(form);
+                return true;
+            } catch(e: any) {
+                return false;
+            }
+        })();
+    }, [form]);
+
+    const canSave = isJsonValid;
+
+    return (
+        <>
+            <Dialog>
+                {trigger || (
+                    <DialogTrigger asChild>
+                        <Button
+                            variant="ghost"
+                        ><EditIcon className="w-4 h-4" /></Button>
+                    </DialogTrigger>
+                )}
+                
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{''}</DialogTitle>
+                        <DialogDescription>{''}</DialogDescription>
+                    </DialogHeader>
+
+                    <div className="flex flex-col gap-y-4">
+                        <div>
+                            <CodeEditor
+                                value={form}
+                                language="json"
+                                placeholder="Enter JSON"
+                                onChange={(evn) => setForm(evn.target.value)}
+                                padding={15}
+                                disabled={false}
+                                data-color-mode='light'
+                                style={{
+                                    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                                }}
+                            />
+                        </div>
+                    </div>
+                    
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button
+                                variant="ghost"
+                            >Cancel</Button>
+                        </DialogClose>
+
+                        <DialogClose asChild>
+                            <Button
+                                disabled={!canSave}
+                                onClick={() => save()}
+                            >Save</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+}
