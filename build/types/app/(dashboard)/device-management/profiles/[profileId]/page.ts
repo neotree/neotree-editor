b1d876bf9dfedabe9236f1bf80f3a6085c35917c
farai <farai.matare@bwscloud.tech>
2026-07-20 12:13:20 +0200

    prod build

--- build/types/app/(dashboard)/device-management/profiles/[profileId]/page.ts
+++ build/types/app/(dashboard)/device-management/profiles/[profileId]/page.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/(dashboard)/device-management/profiles/[profileId]/page.tsx
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/(dashboard)/device-management/profiles/[profileId]/page.tsx
 import * as entry from '../../../../../../../app/(dashboard)/device-management/profiles/[profileId]/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
