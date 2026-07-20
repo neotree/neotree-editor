29963125f21246f150aa8a5b3e7e45a75eb3fc20
farai <farai.matare@bwscloud.tech>
2026-07-20 12:13:20 +0200

    prod build

--- build/types/app/(dashboard)/sessions/[sessionId]/page.ts
+++ build/types/app/(dashboard)/sessions/[sessionId]/page.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/(dashboard)/sessions/[sessionId]/page.tsx
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/(dashboard)/sessions/[sessionId]/page.tsx
 import * as entry from '../../../../../../app/(dashboard)/sessions/[sessionId]/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
