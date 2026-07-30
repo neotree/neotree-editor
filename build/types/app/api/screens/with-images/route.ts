61a79793f26edeb503b59c5401f9fff17c5dbc1b
KumaloWilson <kumalowilson900@gmail.com>
2026-07-30 15:57:59 +0200

    nuid search screens configs

--- build/types/app/api/screens/with-images/route.ts
+++ build/types/app/api/screens/with-images/route.ts
@@ -1,4 +1,4 @@
-// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/screens/with-images/route.ts
+// File: /home/watkay/ReactProjects/neotree-editor/app/api/screens/with-images/route.ts
 import * as entry from '../../../../../../app/api/screens/with-images/route.js'
 import type { NextRequest } from 'next/server.js'
 
