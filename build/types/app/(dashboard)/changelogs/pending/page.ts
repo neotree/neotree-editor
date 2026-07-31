f6893c9ae839aaec114c4ce7c0c841f9ad174246
farai <farai.matare@bwscloud.tech>
2026-07-31 11:26:19 +0200

    prod build

--- build/types/app/(dashboard)/changelogs/pending/page.ts
+++ build/types/app/(dashboard)/changelogs/pending/page.ts
@@ -1,4 +1,4 @@
-// File: /home/watkay/ReactProjects/neotree-editor/app/(dashboard)/changelogs/pending/page.tsx
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/(dashboard)/changelogs/pending/page.tsx
 import * as entry from '../../../../../../app/(dashboard)/changelogs/pending/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
