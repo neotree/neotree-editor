d255f5a54cf970018ac4d6ca9cee64a26ae5486d
KumaloWilson <kumalowilson900@gmail.com>
2026-08-13 13:03:11 +0200

    reference expression exemption

--- app/api/scripts/keys/route.ts
+++ app/api/scripts/keys/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import logger from "@/lib/logger";
 import { isAuthenticated } from "@/app/actions/is-authenticated";
-import { getScriptsWithItems } from "@/app/actions/scripts";
+import { getScriptsConditionKeys } from "@/app/actions/scripts";
 
 export async function GET(req: NextRequest) {
 	try {
@@ -12,14 +12,14 @@ export async function GET(req: NextRequest) {
 
 		const dataJSON = JSON.parse(req.nextUrl.searchParams.get('data') || '{}');
 
-		const { errors, data: scripts, } = await getScriptsWithItems({ ...dataJSON, });
+		const { errors, data: scripts, } = await getScriptsConditionKeys(dataJSON?.scriptsIds || []);
 
         if (errors?.length) return NextResponse.json({ errors, data: [], }, { status: 200, });
 
 		const data = scripts.map(s => ({
 			dataKeys: s.dataKeys,
 		}));
-		
+
 		return NextResponse.json({ data, }, { status: 200, });
 	} catch(e: any) {
 		logger.error('[GET] /api/scripts/keys', e.message);
