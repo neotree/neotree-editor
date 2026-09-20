d63410ccd28e5e91077ee6f090469695e4d61f5a
farai <farai.matare@bwscloud.tech>
2026-09-20 13:28:14 +0200

    prod build

--- build/types/app/api/files/delete-and-replace/route.ts
+++ build/types/app/api/files/delete-and-replace/route.ts
@@ -1,4 +1,4 @@
-// File: /home/wilson/development/neotree/neotree-editor/app/api/files/delete-and-replace/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/files/delete-and-replace/route.ts
 import * as entry from '../../../../../../app/api/files/delete-and-replace/route.js'
 import type { NextRequest } from 'next/server.js'
 
--- build/types/app/api/files/references/route.ts
+++ build/types/app/api/files/references/route.ts
@@ -1,4 +1,4 @@
-// File: /home/wilson/development/neotree/neotree-editor/app/api/files/references/route.ts
+// File: /home/farai/Workbench/Neotree/neotree-editor/app/api/files/references/route.ts
 import * as entry from '../../../../../../app/api/files/references/route.js'
 import type { NextRequest } from 'next/server.js'
 
